8e2a273e1ddfbbc82d81b77da4bac28ece5eb710
lyxshike <543421638@qq.com>
2019-01-14 22:03:41 +0800

    1
    1

---
+++ microservicecloud1/microservicecloud-api/src/test/java/test/TestLombok.java
@@ -0,0 +1,12 @@
+package test;
+
+import com.atguigu.springcloud.entities.Dept;
+
+public class TestLombok {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dept dept = new Dept();
+	}
+
+}
